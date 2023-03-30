341b52ea582e0be522aeb8cda04d30f601963de2
Ameen-vv <mohammedameenvv@gmail.com>
2023-03-30 11:23:15 +0530

    backend hosted

--- client/apiLinks/apiLinks.js
+++ client/apiLinks/apiLinks.js
@@ -1,6 +1,10 @@
 
- export const userUrl='http://localhost:2000/' 
- export const doctorUrl = 'http://localhost:2000/doctor/'
- export const adminUrl ='http://localhost:2000/admin/'
+//  export const userUrl='http://localhost:2000/' 
+//  export const doctorUrl = 'http://localhost:2000/doctor/'
+//  export const adminUrl ='http://localhost:2000/admin/'
+
+ export const userUrl='https://ecare-backend2.onrender.com/' 
+ export const doctorUrl = 'https://ecare-backend2.onrender.com/doctor/'
+ export const adminUrl ='https://ecare-backend2.onrender.com/admin/'
 
 

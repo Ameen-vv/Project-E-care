8a6c33c3de2828f90212a4216c0de20141fe2afd
Ameen-vv <mohammedameenvv@gmail.com>
2023-03-30 16:44:35 +0530

    price fix

--- client/src/components/DoctorSingle/DoctorDetails.jsx
+++ client/src/components/DoctorSingle/DoctorDetails.jsx
@@ -38,7 +38,7 @@ const DoctorSinglePage = () => {
                     <p className="text-sm text-gray-500 mb-2 flex "><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 my-auto">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M15 8.25H9m6 3H9m3 6l-3-3h1.5a3 3 0 100-6M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
-                      : ${doctor.priceOffline} Consultant</p>
+                      : ₹ {doctor.priceOffline} Consultant</p>
 
                   </div>
                   <div>
